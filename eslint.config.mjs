b6f4c4fc229889bc2a64dc8aafcfad2e1499d00a
adams <adamskiptalam0@gmail.com>
2025-01-08 21:00:46 +0300

    new configs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,20 +1,18 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 import eslintConfigPrettier from "eslint-config-prettier";
+
 /** @type {import('eslint').Linter.Config[]} */
 export default [
+  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
+  { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
   {
-    languageOptions: { globals: { ...globals.browser, ...globals.node } },
     rules: {
       "no-unused-vars": "warn",
       "no-undef": "warn",
+      "arrow-body-style": ["warn", "as-needed"],
     },
-    "extends": [
-      "some-other-config-you-use",
-      "prettier"
-  ]
   },
-
   pluginJs.configs.recommended,
   eslintConfigPrettier,
 ];
